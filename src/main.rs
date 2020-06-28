3d327a092ad54fa95a07cd50ee81688f74bab0dd
Xiangdong Zeng <xdzeng96@gmail.com>
2020-06-28 18:18:03 +0800

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
 use rustotf;
-use std::process;
+use std::{env, process};
 
 fn main() {
-    for font_file_name in &[
-        // Basic
-        "./test/fonts/macos/Arial.ttf",
-        "./test/fonts/msword/times.ttf",
-        "./test/fonts/texlive/lmroman10-regular.otf",
-        "./test/fonts/texlive/NotoSans-Regular.ttf",
-        "./test/fonts/texlive/SourceSansPro-RegularIt.otf",
-        // CJK
-        "./test/fonts/contrib/BabelStoneHan.ttf",
-        "./test/fonts/contrib/BabelStoneHanPUA.ttf",
-        "./test/fonts/contrib/HYZhongHeiS.ttf",
-        "./test/fonts/msword/msyh.ttf",
-        "./test/fonts/texlive/FandolSong-Regular.otf",
-        // TTC
-        "./test/fonts/contrib/iosevka-medium.ttc",
-        "./test/fonts/contrib/sarasa-regular.ttc",
-        "./test/fonts/contrib/SourceHanNotoCJK.ttc",
-        "./test/fonts/macos/HelveticaNeue.ttc",
-        "./test/fonts/macos/PingFang.ttc",
-        "./test/fonts/macos/ヒラギノ角ゴシック W4.ttc",
-        "./test/fonts/msword/Cambria.ttc",
-        // Emoji
-        "./test/fonts/contrib/NotoColorEmoji.ttf",
-        "./test/fonts/contrib/NotoEmoji-Regular.ttf",
-        "./test/fonts/macos/Apple Color Emoji.ttc",
-        // Variable fonts
-        "./test/fonts/contrib/AdobeVFPrototype.otf",
-        "./test/fonts/contrib/AdobeVFPrototype.ttf",
-        "./test/fonts/macos/SFNS.ttf",
-        // WOFF
-        // WOFF2
-    ] {
-        if let Err(e) = rustotf::read_font(font_file_name) {
-            eprintln!("Application error: {}", e);
-            process::exit(1);
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        match rustotf::read_font(&args[1]) {
+            Err(e) => {
+                eprintln!("Application error: {}", e);
+                process::exit(1);
+            },
+            _ => (),
         }
     }
 }
---
+++ tests/test.rs
@@ -0,0 +1,41 @@
+use rustotf::FontContainer;
+use std::fs;
+
+const FONTS_PATH : &str = "./tests/fonts/";
+
+const TTF_FONTS: [&str; 3] = [
+    "NotoSans-SemiCondensed.ttf",
+    "SourceSerifPro-LightIt.ttf",
+    "WorkSans-Regular.ttf",
+];
+
+fn check_font(font_file_path: &str) {
+    println!("Checking font: {}", font_file_path);
+
+    let mut font_container = FontContainer::new(fs::read(font_file_path).unwrap());
+    font_container.init();
+    assert_ne!(font_container.fonts.len(), 0);
+
+    for table in &["head", "hhea", "maxp", "hmtx", "cmap", "name", "OS/2", "post"] {
+        font_container.parse_table(table);
+    }
+
+    for font in font_container.fonts {
+        assert!(font.head.is_some());
+        assert!(font.hhea.is_some());
+        assert!(font.maxp.is_some());
+        assert!(font.hmtx.is_some());
+        assert!(font.cmap.is_some());
+        assert!(font.name.is_some());
+        assert!(font.OS_2.is_some());
+        assert!(font.post.is_some());
+    }
+}
+
+#[test]
+fn check_ttf() {
+    for i in &TTF_FONTS {
+        let font_file_name = [FONTS_PATH, i].join("");
+        check_font(&font_file_name);
+    }
+}
