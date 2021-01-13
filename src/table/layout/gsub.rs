73773f89f045d68c3126b7d2cf8ab388c2a94de0
Xiangdong Zeng <xdzeng96@gmail.com>
2021-01-13 14:10:34 +0800

    GSUB: features and lookups

--- src/table/layout/gsub.rs
+++ src/table/layout/gsub.rs
@@ -17,6 +17,8 @@ use read_buffer_derive::ReadBuffer;
 pub struct Table_GSUB {
     _version: String,
     pub script_list: Vec<ScriptRecord>,
+    pub feature_list: Vec<FeatureRecord>,
+    pub lookup_list: Vec<Lookup>,
 }
 
 impl Font {
@@ -38,18 +40,38 @@ impl Font {
         let num_scripts: u16 = buffer.get();
         let mut script_list: Vec<ScriptRecord> = buffer.get_vec(num_scripts as usize);
         script_list.iter_mut().for_each(|rec| {
-            let script_start_offset = script_list_start_offset + rec.script_offset as usize;
-            buffer.offset = script_start_offset;
+            buffer.offset = script_list_start_offset + rec.script_offset as usize;
             rec.script = buffer.get();
         });
 
-        println!("feature_list_offset       = {}", feature_list_offset);
-        println!("lookup_list_offset        = {}", lookup_list_offset);
+        let feature_list_start_offset = gsub_start_offset + feature_list_offset as usize;
+        buffer.offset = feature_list_start_offset;
+        let num_features: u16 = buffer.get();
+        let mut feature_list: Vec<FeatureRecord> = buffer.get_vec(num_features as usize);
+        feature_list.iter_mut().for_each(|rec| {
+            buffer.offset = feature_list_start_offset + rec.feature_offset as usize;
+            rec.feature = buffer.get();
+        });
+
+        let lookup_list_start_offset = gsub_start_offset + lookup_list_offset as usize;
+        buffer.offset = lookup_list_start_offset;
+        let num_lookups: u16 = buffer.get();
+        let lookup_offsets: Vec<u16> = buffer.get_vec(num_lookups as usize);
+        let lookup_list = lookup_offsets
+            .iter()
+            .map(|&offset| {
+                buffer.offset = lookup_list_start_offset + offset as usize;
+                buffer.get()
+            })
+            .collect();
+
         println!("feature_variations_offset = {:?}", feature_variations_offset);
 
         self.GSUB = Some(Table_GSUB {
             _version,
             script_list,
+            feature_list,
+            lookup_list,
         });
     }
 }
@@ -125,11 +147,72 @@ impl ReadBuffer for LangSys {
     fn read(buffer: &mut Buffer) -> Self {
         buffer.skip::<u16>(1); // lookupOrderOffset = NULL
         let required_feature_index = buffer.get();
-        let feature_index_count = buffer.get::<u16>() as usize;
-        let feature_indices = buffer.get_vec(feature_index_count);
+        let feature_index_count: u16 = buffer.get();
+        let feature_indices = buffer.get_vec(feature_index_count as usize);
         Self {
             required_feature_index,
             feature_indices,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct FeatureRecord {
+    pub feature_tag: Tag,
+    pub feature: Feature,
+    feature_offset: u16,
+}
+
+impl ReadBuffer for FeatureRecord {
+    fn read(buffer: &mut Buffer) -> Self {
+        Self {
+            feature_tag: buffer.get(),
+            feature_offset: buffer.get(),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Feature {
+    pub feature_params_offset: u16,
+    pub lookup_list_indices: Vec<u16>,
+}
+
+impl ReadBuffer for Feature {
+    fn read(buffer: &mut Buffer) -> Self {
+        let feature_params_offset = buffer.get();
+        let lookup_index_count: u16 = buffer.get();
+        let lookup_list_indices = buffer.get_vec(lookup_index_count as usize);
+        Self {
+            feature_params_offset,
+            lookup_list_indices,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Lookup {
+    pub lookup_type: u16,
+    pub lookup_flag: u16,
+    subtable_count: u16,
+    subtable_offsets: Vec<u16>,
+    pub mark_filtering_set: u16,
+}
+
+impl ReadBuffer for Lookup {
+    fn read(buffer: &mut Buffer) -> Self {
+        let lookup_type = buffer.get();
+        let lookup_flag = buffer.get();
+        let subtable_count = buffer.get();
+        let subtable_offsets = buffer.get_vec(subtable_count as usize);
+        let mark_filtering_set = buffer.get();
+        Self {
+            lookup_type,
+            lookup_flag,
+            subtable_count,
+            subtable_offsets,
+            mark_filtering_set,
         }
     }
 }
